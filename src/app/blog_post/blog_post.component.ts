3c7ac980720cd5126353e388ff228d6fc1a3ae55
vietnguyen <vietnguyen@absolute.com>
2017-03-03 17:08:17 +0700

    init

--- src/app/blog_post/blog_post.component.ts
+++ src/app/blog_post/blog_post.component.ts
@@ -4,16 +4,10 @@ import {FormControl} from '@angular/forms';
 @Component({
   selector: 'about',
   template: `
-<div class="container">
-	<h1>Using ngModel</h1>
-	<app-tinymce [(ngModel)]='content'></app-tinymce>
-	<pre>{{content}}</pre>
-
-
-	<h1>Using formControl</h1>
-	<app-tinymce [formControl]='contentControl'></app-tinymce>
-	<pre>{{contentControl.value}}</pre>
+<div style="height: 500px">
+<app-tinymce [(ngModel)]='content'></app-tinymce>
 </div>
+
 `,
   encapsulation: ViewEncapsulation.None
 })
