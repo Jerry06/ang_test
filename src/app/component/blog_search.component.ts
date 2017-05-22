58f4b02784cd0939ebdc9e71f4f41061579fa669
viet <nxviet06@gmail.com>
2017-05-23 06:37:41 +0700

    init

--- src/app/component/blog_search.component.ts
+++ src/app/component/blog_search.component.ts
@@ -2,13 +2,22 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'blog_search',
-  templateUrl : 'blog_search.component.html',
+  templateUrl: 'blog_search.component.html',
 })
 export class SearchComponent implements OnInit {
+
+  searchText: string;
+
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  search() {
+    if (this.searchText.trim()){
+      alert(this.searchText);
+    }
   }
 
 }
--- src/app/component/footer.component.ts
+++ src/app/component/footer.component.ts
@@ -2,17 +2,7 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'footer',
-  //templateUrl: 'src/app/component/footer.component.html'
-  template: `
-<div class="row">
-  <div class="col-lg-12">
-    <p>Copyright &copy; Your Website 2014</p>
-  </div>
-  <!-- /.col-lg-12 -->
-</div>
-
-
-`
+  templateUrl: './footer.component.html'
 })
 export class FooterComponent {
 }
